2c798958b80766d93fb93443b7147570df1969e7
denisadudas <dudasdenisaandrea@yahoo.ro>
2025-05-27 22:01:58 +0200

    created the get rest api

---
+++ src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
@@ -0,0 +1,25 @@
+package fr.esgi.rent.api;
+
+import fr.esgi.rent.domain.RentalPropertyEntity;
+import fr.esgi.rent.repository.RentalPropertyRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class RentalPropertyResource {
+
+    private final RentalPropertyRepository rentalPropertyRepository;
+
+    public RentalPropertyResource(RentalPropertyRepository rentalPropertyRepository) {
+        this.rentalPropertyRepository = rentalPropertyRepository;
+    }
+
+    @GetMapping("/rental-properties")
+    public List<RentalPropertyEntity> getRentalProperties() {
+        return rentalPropertyRepository.findAll();
+    }
+}
